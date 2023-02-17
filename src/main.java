3b3a6633a33624185e617f0352a9d2dc4bb43605
omarshalash1 <124627562+omarshalash1@users.noreply.github.com>
2023-02-17 10:59:05 -0500

    Update main.java
    comments added to the code

--- src/main.java
+++ src/main.java
@@ -1,16 +1,23 @@
+// This code imports the Scanner class from the java.util package.
 import java.util.Scanner;
 
+// This line declares a public class called "main".
 public class main {
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
+// This line declares a static void method called "main" that takes in an array of Strings as its argument.
+public static void main(String[] args) {
+// This line creates a new Scanner object called "scan".
+Scanner scan = new Scanner(System.in);
+  // This line prints a message asking the user to enter their birth month.
+System.out.print("Enter your birth month (1-12): ");
 
-    System.out.print("Enter your birth month (1-12): ");
-    int birthMonth = 15;
+// This line initializes an integer variable called "birthMonth" to 15. (Note: this value is hard-coded and not based on user input.)
+int birthMonth = 15;
 
-    if (birthMonth >= 1 && birthMonth <= 12) {
-      System.out.println("Your birth month is: " + birthMonth);
-    } else {
-      System.out.println("You entered an incorrect month value: " + birthMonth);
-    }
-  }
+// This line begins an if-else statement. If the value of birthMonth is between 1 and 12 (inclusive), it is considered a valid month value and a message is printed indicating the birth month. Otherwise, an error message is printed with the value of birthMonth.
+if (birthMonth >= 1 && birthMonth <= 12) {
+  System.out.println("Your birth month is: " + birthMonth);
+} else {
+  System.out.println("You entered an incorrect month value: " + birthMonth);
+}
+}
 }
